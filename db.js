290dc521f9b62ff0b8ca2c2bac0ff78048a87325
Jean-Philippe Ricard <ricardjp@arcanix.com>
2016-08-15 23:01:57 -0400

    db is using localhost by default

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongojs = require('mongojs')
 
-var db = mongojs(process.env.IP + '/oauth2orize_authorization_grant_example')
+var db = mongojs('127.0.0.1/oauth2orize_authorization_grant_example')
 
 exports.db = function() {
     return db
